f63ac02c30b331619908ab15d77e4f1a95112821
whatevermane <zmatt33@gmail.com>
2019-04-24 16:09:10 -0500

    Initial files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,4 @@
+//Your htmlRoutes.js file should include two routes:
+
+//1.A GET Route to /survey which should display the survey page.
+//2.A default, catch-all route that leads to home.html which displays the home page.
