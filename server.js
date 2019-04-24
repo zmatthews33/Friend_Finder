2fdc87344f0882497f84f5078657a95251bd1f25
whatevermane <zmatt33@gmail.com>
2019-04-24 17:29:23 -0500

    Added Express and requires htmlRoutes

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require("express");
+
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var htmlRoutes = require("./app/routing/htmlRoutes");
+app.use(htmlRoutes);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
